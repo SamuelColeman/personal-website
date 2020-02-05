3a6b086729a3c441c42bea7dff7412cb033e4076
Samuel Coleman <smcoleman19@gmail.com>
2020-02-04 20:55:54 -0700

    Add basic setup for projects and experience

--- src/components/Experience/Experience.js
+++ src/components/Experience/Experience.js
@@ -5,6 +5,9 @@ export default class Experience extends Component {
 	render() {
 		return (
 			<div className="Experience">
+				<div className="exp-header">Education</div>
+				<div className="exp-header">Work</div>
+				<div className="exp-header">Skills</div>
 			</div>
 		)
 	}
--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
 import './Projects.css';
+import Linkify from 'react-linkify';
 
 export default class Projects extends Component {
 	render() {
 		return (
 			<div className="Projects">
+				<div className="project-title">PupDates</div>
+				<Linkify>https://github.com/PupDates-CrossPol</Linkify>
 			</div>
 		)
 	}

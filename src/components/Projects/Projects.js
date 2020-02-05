c8befaa1698a8579175610ceaec583276ddfaa05
Samuel Coleman <smcoleman19@gmail.com>
2020-02-04 21:01:59 -0700

    Add basic styling to projects component

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -1,13 +1,12 @@
 import React, { Component } from 'react';
 import './Projects.css';
-import Linkify from 'react-linkify';
 
 export default class Projects extends Component {
 	render() {
 		return (
 			<div className="Projects">
 				<div className="project-title">PupDates</div>
-				<Linkify>https://github.com/PupDates-CrossPol</Linkify>
+				<a className="project-link" href={"https://github.com/PupDates-CrossPol"}>GitHub</a>
 			</div>
 		)
 	}

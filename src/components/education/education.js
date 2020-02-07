e14afba35a848547d2f9c7c4c02b318b6666e2ed
Samuel Coleman <smcoleman19@gmail.com>
2020-02-06 17:20:48 -0700

    Add basic component setup for education and skills

---
+++ src/components/education/education.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './Education.css';
+
+export default class Education extends Component {
+	render() {
+		return (
+			<div className="Education">
+				<div className="name"></div>
+				<div className="major"></div>
+				<div className="start_date"></div>
+				<div className="end_date"></div> 
+			</div>
+		)
+	}
+}
